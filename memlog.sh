2d80b2a4fdbe1c870fab92bfefd4b0d298b6ec8a
Brian Atkinson <nairb774@gmail.com>
2010-10-11 14:50:39 -0500

    Catch running program numbers, disk activity, swap activity, and cpu %s

--- memlog.sh
+++ memlog.sh
@@ -6,6 +6,7 @@ cd ${0%/*}
     date +%FT%T.%N
     cat /proc/loadavg
     free -o | tail -n+2 | sed -r 's/\s+/\t/g' | cut -f2-7
+    vmstat 1 2 | tail -n1 | sed -r 's/\s+/\t/g' | cut -f2,3,8-17
   } | tr '\n' '\t'
   echo
 } | sed -r 's/\s+/\t/g' >> memlog
